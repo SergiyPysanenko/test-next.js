9d77285ad4715199b38682e7459ec895e9c75496
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-09-20 21:41:14 +0300

    add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
+import Link from 'next/link';
+import {BsFacebook, BsInstagram } from 'react-icons/bs'
+import s from './Footer.module.css'
+
+
 
 const Footer = () => {
     return (
-        <div>
-            <footer>
-                FOOTER
+        <>
+            <footer className={s.footer_section}>
+                <span className={s.span_logo}>LOGO</span>
+                    <Link href={"/"}  className={s.link_fb}>
+                        <BsFacebook size={25} />
+                    </Link>
+                    <Link href={"/"} className={s.link_ig}>
+                        <BsInstagram size={25} />
+                    </Link>
             </footer>
-        </div>
+        </>
     )
 };
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,15 +5,11 @@ import Image from 'next/image'
 const Main = (props) => {
     return(
         <>
-        <div>
-            <section className={styles.sectionMain} >
-                <h1 className={styles.sectionText}>Всім привіт! Мене зовуть Вікторія, і я крутий прсихолог</h1>
-                    <div className={styles.mainWrapperP}>
-                        <p className={styles.text_p}>Цей текст буде тут!!!</p>
-                        <Image className={styles.image_main} src='/main_image.png' alt="maim image" width={523} height={632} /> 
-                    </div>
-            </section>
-        </div>         
+            <section className={styles.flex_container} >
+                <h1 className={styles.text_container}>Мене зовуть Вікторія, і я крутий психолог!</h1>
+                        <Image className={styles.image_main} src='/main_image.png' alt="maim image" width={523} height={632} />
+                        <p className={styles.text_p}>Цей текст буде тут!!!11111111111</p>
+            </section>        
         </>
     );            
 };
